4b6d4bcbfef3e25ffe168ab16eb6950183f5e534
ransel07 <ranselysandro@gmail.com>
2022-12-05 19:18:25 -0400

    dicc

--- PY/diccionario.py
+++ PY/diccionario.py
@@ -2,4 +2,7 @@ diccionario = {"Sapiens" : "Yuval Harary", "Python para todos":"Ranúl Gonzales 
 #En este caso "Sapiens" y "Python para todos" son denominados Key o Clave
 #Y "Yuval Harary" y "Raul Gonzales Duque" se pueden denominar dato o valor.
 
-print(diccionario["Sapiens"], "y", diccionario["Python para todos"])
+def diccionarios ():
+    print(diccionario["Sapiens"], "y", diccionario["Python para todos"])
+
+diccionarios()
