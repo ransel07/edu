0b0da3676d91da0a223a475745c75e3c0ac050e3
Ransel Melenciano <ranselysandro@gmail.com>
2022-12-16 22:08:44 -0400

    Ejercicios de registros

--- PY/ejercode/Seccion 6/Registros.py
+++ PY/ejercode/Seccion 6/Registros.py
@@ -1,70 +1,17 @@
 # EJECICIOS DE REGISTROS
-
-persona = {
-    'nombre': 'Juan',
-    'edad': 35,
-    'direccion': 'Calle Falsa 123'
-}
-
-libro = {
-    'titulo': 'El señor de los anillos',
-    'autor': 'J.R.R. Tolkien',
-    'año': 1954,
-    'genero': 'Fantasía'
-}
-
-pelicula = {
-    'titulo': 'Star Wars',
-    'director': 'George Lucas',
-    'ano': 1977,
-    'actores': ['Mark Hamill', 'Harrison Ford', 'Carrie Fisher']
-}
-
-# Crea un procedimiento llamado "imprimir_persona" que reciba como parámetro una variable de tipo "persona" y muestre en 
-# pantalla los valores de sus campos. 
-
-def imprimir_persona(persona):
-    print(persona["nombre"])
-    print(persona["edad"])
-    print(persona["direccion"])
+import regis_ejemp
 
 #imprimir_persona(persona)
 
 # Crea un registro llamado "persona" que contenga los campos "nombre", "edad" y "dirección". Luego, crea una variable de 
 # este tipo de registro y asígnale valores a sus campos.
 
-persona["nombre"] = "Ransel"
-persona["edad"] = 27
-persona["direccion"] = "calle puerto rico #14"
+
 
 # Crea una función llamada "edad_promedio" que reciba como parámetro un arreglo de variables de tipo "persona" y devuelva 
 # como resultado la edad promedio de todas las personas en el arreglo.
 
-persona1 = {
-    "nombre" : "Rodrigo",
-    "edad" : 33,
-    "direccion" : "una calle hay numero algo"
-}
 
-persona2 = {
-    "nombre" : "ana",
-    "edad" : 28,
-    "direccion" : "calle 13"
-}
-
-persona3 = {
-    "nombre" : "Lorine",
-    "edad" : 25,
-    "direccion" : "zona colonial"
-}
-
-persona4 = {
-    "nombre" : "Daneyris",
-    "edad" : 30,
-    "direccion" : "kings Landing"
-}
-
-personas = [persona["edad"], persona1["edad"], persona2["edad"], persona3["edad"], persona4["edad"]]
 def edad_promedio(personas):
     sum = 0
     for edad in personas:
@@ -77,9 +24,7 @@ def edad_promedio(personas):
 # Crea un registro llamado "libro" que contenga los campos "título", "autor" y "año_publicacion". Luego, crea una variable 
 # de este tipo de registro y asígnale valores a sus campos.
 
-libro["titulo"] = "Harry Potter y el Prisionero de Askaban"
-libro["autor"] = "J.K. Rolling"
-libro["año"] = 1999
+
 
 # Crea un procedimiento llamado "imprimir_libro" que reciba como parámetro una variable de tipo "libro" y muestre en pantalla 
 # los valores de sus campos.
@@ -91,40 +36,6 @@ def imprimir_libro(libro):
 
 # Crea una función llamada "libros_por_autor" que reciba como parámetros un arreglo de variables de tipo "libro" y un nombre de 
 # autor, y devuelva como resultado un arreglo con todos los libros escritos por ese autor.
-
-libro1 = {
-    'titulo': 'El señor de los anillos',
-    'autor': 'J.R.R. Tolkien',
-    'año': 1954,
-    'genero': 'Fantasía'
-}
-
-libro2 = {
-    'titulo': 'Game of Throne',
-    'autor': 'George R. R. Martin',
-    'año': 1996,
-    'genero': 'Fantasía Medival'
-}
-
-libro3 = {
-    'titulo': 'Los juegos del hambre',
-    'autor': 'Suzanne Collins',
-    'año': 2008,
-    'genero': 'Ficción adulto joven'
-}
-
-libro4 = {
-    'titulo': 'Danza de dragones',
-    'autor': 'George R. R. Martin',
-    'año': 2011,
-    'genero': 'Fantasía Medival'
-}
-
-libros_autores = [libro["autor"],libro1["autor"],libro2["autor"],libro3["autor"],libro4["autor"]]
-libros_titulo = [libro["titulo"],libro1["titulo"],libro2["titulo"],libro3["titulo"],libro4["titulo"]]
-libros_titulo1 = [libro, libro1, libro2, libro3, libro4]
-lbn = [libro]
-autor = libro['autor']
 
 def libros_por_autor(libros, nombre_autor):
     libros_del_autor = []
@@ -151,55 +62,14 @@ def libros_por_año(libros, año):
 
 #libros_por_año(libros_titulo1, libro2["año"])
 
-# Crea un registro llamado "estudiante" que contenga los campos "nombre", "edad", "promedio" y "becado". Luego, 
-# crea una variable de este tipo de registro y asígnale valores a sus campos.
 
-estudiante = {
-    "nombre" : "",
-    "edad" : "",
-    "promedio" : "",
-    "becado" : ""
-}
-
-estudiante1 = {
-    "nombre" : "Rodrigo",
-    "edad" : 33,
-    "promedio" : 2.9,
-    "becado" : "si"
-}
-
-estudiante2 = {
-    "nombre" : "ana",
-    "edad" : 17,
-    "promedio" : 4.0,
-    "becado" : "si"
-}
-
-estudiante3 = {
-    "nombre" : "Lorine",
-    "edad" : 25,
-    "promedio" : 2.0,
-    "becado" : "no"
-}
-
-estudiante4 = {
-    "nombre" : "Daneyris",
-    "edad" : 30,
-    "promedio" : 3.0,
-    "becado" : "si"
-}
-
-estudiante["nombre"] = "David" 
-estudiante["edad"] = 30 
-estudiante["promedio"] = 4.0 
-estudiante["becado"] = "si"
 
 #print(estudiante)
 
 # Crea una función llamada "mayores_edad" que reciba como parámetro un arreglo de variables de tipo "estudiante" 
 # y devuelva como resultado un arreglo con todos los estudiantes mayores de edad (edad >= 18).
 
-lista_estudiantes = [estudiante,estudiante1,estudiante2,estudiante3,estudiante4]
+
 
 def mayores_edad(variabble_estudiante):
     adultos = []
@@ -246,31 +116,9 @@ def becados(variabble_estudiante):
 # precio (flotante) y cantidad (entero). Luego, cree una lista de productos y utilice un ciclo para calcular el total 
 # de la compra de cada producto en la lista.
 
-producto = {
-    "nombre" : "Ram",
-    "precio" : 75.5,
-    "cantidad" : 1,
-}
 
-producto1 = {
-    "nombre" : "Board",
-    "precio" : 200.0,
-    "cantidad" : 1,
-}
 
-producto2 = {
-    "nombre" : "Tarjeta de Video",
-    "precio" : 298.3,
-    "cantidad" : 1,
-}
 
-producto3 = {
-    "nombre" : "SSD m.2",
-    "precio" : 93.8,
-    "cantidad" : 1,
-}
-
-lista_productos = [producto, producto1, producto2, producto3]
 
 def carrito(productos):
     sum = 0
@@ -278,38 +126,56 @@ def carrito(productos):
         sum = sum + elemento["precio"]
     print(sum)
 
-carrito(lista_productos)
+carrito(regis_ejemp.lista_productos)
 
-#Crea una estructura de datos para representar una canción, con los siguientes campos: título (cadena de caracteres), 
-# artista (cadena de caracteres) y duración (flotante en segundos). Luego, cree una lista de canciones y utilice un 
-# ciclo para calcular la duración total de todas las canciones en la lista.
-
-
-
-#Crea una estructura de datos para representar un estudiante, con los siguientes campos: nombre (cadena de caracteres), 
+# Crea una estructura de datos para representar un estudiante, con los siguientes campos: nombre (cadena de caracteres), 
 # edad (entero), curso (cadena de caracteres) y calificación (flotante). Luego, cree una lista de estudiantes y utilice 
 # un ciclo para calcular el promedio de calificación de todos los estudiantes en la lista.
 
-def promedio_promedios(estudiantes):
-    for est in estudiantes:
-        estudiantes.count(est[""])
 
-#Crea una estructura de datos para representar una persona, con los siguientes campos: nombre (cadena de caracteres), 
+def promedio_promedios(estudiantes):
+    cant = len(estudiantes)
+    sum = 0
+    lista_promedios = []
+    for est in estudiantes:
+        contador = 0
+        switch = True
+        while switch:
+            contador += 1
+            if contador == est["promedio"]:
+                lista_promedios.append(contador)
+                switch = False
+    for est2 in lista_promedios:                    # Luego de poner los promedios en una lista, se suman en este bucle
+        sum =  sum + est2
+    promedio = sum / cant
+    print (lista_promedios)
+
+#promedio_promedios(lista_estudiantes)
+
+
+
+# Crea una estructura de datos para representar una persona, con los siguientes campos: nombre (cadena de caracteres), 
 # edad (entero), dirección (cadena de caracteres), ciudad (cadena de caracteres) y estado (cadena de caracteres). 
 # Luego, cree un diccionario de personas, donde la clave sea el nombre de la persona y el valor sea el registro de 
 # la persona. Utiliza un ciclo para imprimir cada una de las personas en el diccionario.
 
-#Crea una estructura de datos para representar un producto, con los siguientes campos: nombre (cadena de caracteres), 
+def imprimir(directorio):
+    for indiv in directorio:
+        for indiv2 in indiv:
+            print(indiv2)
+
+
+# Crea una estructura de datos para representar un producto, con los siguientes campos: nombre (cadena de caracteres), 
 # precio (flotante) y cantidad (entero). Luego, cree un diccionario de productos, donde la clave sea el nombre del 
 # producto y el valor sea el registro del producto. Utiliza un ciclo para calcular el total de la compra de cada 
 # producto en el diccionario.
 
-#Crea una estructura de datos para representar una canción, con los siguientes campos: título (cadena de caracteres), 
+# Crea una estructura de datos para representar una canción, con los siguientes campos: título (cadena de caracteres), 
 # artista (cadena de caracteres) y duración (flotante en segundos). Luego, cree un diccionario de canciones, donde la 
 # clave sea el título de la canción y el valor sea el registro de la canción. Utiliza un ciclo para calcular la 
 # duración total de todas las canciones en el diccionario.
 
-#Crea una estructura de datos para representar un estudiante, con los siguientes campos: nombre (cadena de caracteres), 
+# Crea una estructura de datos para representar un estudiante, con los siguientes campos: nombre (cadena de caracteres), 
 # edad (entero), curso (cadena de caracteres) y calificación (flotante). Luego, cree un diccionario de estudiantes, 
 # donde la clave sea el nombre del estudiante y el valor sea el registro del estudiante. Utiliza un ciclo para calcular 
 # el promedio de calificación de todos los estudiantes en el diccionario.
