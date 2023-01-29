dbe3ac5643adea989a4a8b4d0c48c99c9eda9bc3
Ransel Melenciano <51245246+ransel07@users.noreply.github.com>
2023-01-28 21:20:39 -0400

    Anaconda (1.1)

--- Anaconda/prub_mach_learn.py
+++ Anaconda/prub_mach_learn.py
@@ -12,12 +12,48 @@ import statsmodels.api as sm
 url = 'https://raw.githubusercontent.com/JoaquinAmatRodrigo/Estadistica-machine-learning-python/master/data/Howell1.csv'
 read = pd.read_csv(url, sep=",")
 
-#print(read.head(5))
+print(read.head(6))
 #print ("Cantidad de Filas y columnas: ", read.shape)
 #print ("Nombre columnas: ", read.columns)
 #print (read.info())
 #print (read.describe())
 
-corr = read.set_index("age").corr()
-sm.graphics.plot_corr(corr, xnames=list(corr.columns))
-plt.show()
+# CORRELACION
+
+# corr = read.set_index("age").corr()
+# sm.graphics.plot_corr(corr, xnames=list(corr.columns))
+# plt.show()
+
+pop = read[read["height"] > 150]
+print (pop.head(6))
+
+pop1 = read[read["weight"] > 50]
+pop.drop(['height'],axis=1)['age'].plot(kind='bar')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
