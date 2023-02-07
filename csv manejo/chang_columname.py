5cca730199a72e9553fab7892275a94343058d48
Ransel Melenciano <ranselysandro@gmail.com>
2023-02-06 21:05:09 -0400

    .

--- csv manejo/chang_columname.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-path = "C:/Users/rdrs_/OneDrive/Documentos/GitHub/Ste/Anaconda/CuentaMovimientos_.csv"
-df = pd.read_csv(path)
-df = df.rename(columns={'Balance  ': 'Balance'})
-df.to_csv("CuentaMovimientos_.csv", index=False)
